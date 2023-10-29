7e29ab064ffffcf8e333a030eec615af05cf3554
William Yang <yangw031@gmail.com>
2023-10-29 16:24:06 -0400

    Session State Troubles

--- src/App.js
+++ src/App.js
@@ -1,36 +1,60 @@
 import { useState } from 'react';
 
+
 //////////////////////////////////////// BUTTONS ////////////////////////////////////////
 
 function Button({value, onAddRowClick}) {
   return (
-    <button className="add-row" onClick={onAddRowClick}>
+    <button className="button" onClick={onAddRowClick}>
       {value}
     </button>
   );
 }
 
-function Buttons() {
-  let rows = 0;   // Rows counter
-  let cols = 0;   // Columns counter
+function ColorPicker({color, onChange}) {
+  return (
+    <input type="color" color={color} onChange={onChange}/>
+  );
+}
 
-  function AddRow(value) {
-    return (
-      <button className="add-ro">
-        {value}
-      </button>
-    );
+function Buttons() {
+  console.log('start');
+  const [rows, setRows] = useState(0);   // Rows counter
+  const [cols, setCols] = useState(0);   // Columns counter
+  const [color, setColor] = useState("#00FF00");
+  console.log('vars');
+
+  function AddRow() {
+    setRows(rows + 1);
+    console.log(rows);
   }
+
+  function AddCol() {
+    setCols(cols + 1);
+    console.log(cols);
+  }
+
+  function RemRow() {
+    setRows(rows - 1);
+    console.log(rows);
+  }
+
+  function RemCol() {
+    setCols(cols - 1);
+    console.log(cols);
+  }
+
+  
 
   return (
     <>
       <div className="buttons">
-        <Button value={"Add Row"} onAddRowClick={() => AddRow(0)}/>
-        <Button value={"Add Column"} onAddRowClick={() => AddRow(0)}/>
-        <Button value={"Remove Row"} onAddRowClick={() => AddRow(0)}/>
-        <Button value={"Remove Column"} onAddRowClick={() => AddRow(0)}/>
+        <Button value={"Add Row"} onAddRowClick={() => AddRow()}/>
+        <Button value={"Add Column"} onAddRowClick={() => AddCol()}/>
+        <Button value={"Remove Row"} onAddRowClick={() => RemRow()}/>
+        <Button value={"Remove Column"} onAddRowClick={() => RemCol()}/>
 
-        <input type="color" id="color-picker" value="#00FF00"></input>
+        <ColorPicker value="color" color={color} onChange={setColor}/>
 
         <Button value={"Color All Cells"} onAddRowClick={() => AddRow(0)}/>
         <Button value={"Color All Uncolored Cells"} onAddRowClick={() => AddRow(0)}/>
@@ -44,7 +68,9 @@ function Buttons() {
 
 //////////////////////////////////////// GRID ////////////////////////////////////////
 
-function Grid() {
+function Grid({rows, cols}) {
+
+
   return (
     <table className="table">
       
@@ -57,12 +83,13 @@ function Grid() {
 //////////////////////////////////////// MAIN ////////////////////////////////////////
 
 export default function Main() {
-  const [rows, setRows] = useState(0);   // Rows counter
-  const [cols, setCols] = useState(0);   // Columns counter
+  // const [rows, setRows] = useState(0);   // Rows counter
+  // const [cols, setCols] = useState(0);   // Columns counter
+  // const [color, setColor] = useColor
 
-  function updateRows(value) {
-    setRows(value)
-  }
+  // function updateRows(value) {
+  //   setRows(value)
+  // }
 
   return (
     <div className="main">
@@ -74,5 +101,5 @@ export default function Main() {
         <Grid/>
       </div>
     </div>
-  )
+  );
 }
